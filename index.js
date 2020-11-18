1c64db1cfccf1eca0f261a5c9b2d2a2dfa588888
Will-Gon <will.colocho@gmail.com>
2020-11-17 22:23:43 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let noLonger = document.querySelector('main#main')
+noLonger.remove(main)
+
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Will-Gon is the champion'
